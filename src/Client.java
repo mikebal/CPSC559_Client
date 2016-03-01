b24e08965226f99245c2bd92878ac926ade2858a
RichardMGame <richardmgame@gmail.com>
2016-03-01 11:01:08 -0700

    added some basic IO handling for the client along wih create a command-line parameter to allow users to self-identify themselves.
    Operation:
    1. New user gets creates at the beginning with a identifier
    2. New user has a list of files that they have; all new users start with
    no files
    3.new client then broadcasts identity and files they own to Tracker. 
    4. client then has choice of input commands: add, get, and exit. (all
    self-explanatory"

--- src/Client.java
+++ src/Client.java
@@ -1,67 +1,75 @@
 import java.io.*;
 import java.net.*;
 import java.util.*;
-public class Client
-{
-    public Client()
-    {
 
-    }
+public class Client {
 
-    public static void main(String[] args)
-    {
-        Socket  clientSocket = null;
-        InputStream     is           = null;
-        OutputStream    os           = null;
+	public static void main(String[] args) {
+		Socket clientSocket = null;
+		InputStream is = null;
+		OutputStream os = null;
 
-        //attempt to connect to the server
-        try
-        {
-            //create a socket
-            clientSocket = new Socket("localhost", 9000);
-            try
-            {
-                //get the input and output streams
-                os = clientSocket.getOutputStream();
-                is = clientSocket.getInputStream();
-                Scanner in = new Scanner(is);
-                PrintWriter out  = new PrintWriter(os, true /* autoflush */);
-                Scanner userIn = new Scanner(System.in);
+		String id = null;
+		if (args.length == 0) {
+			id = "JohnDoe";
+		} else
+			id = args[0];
+		ClientInfo ci = new ClientInfo(id);
 
-               // String userLine = "";
-               // System.out.println("Enter data to send to the SERVER");
-               // while(userIn.hasNextLine())
-               // {
-               //     userLine = userIn.nextLine();
-                    out.println("This is Dog");
-                 //   out.flush();
-                //}
+		// attempt to connect to the server
+		try {
+			// create a socket
+			clientSocket = new Socket("localhost", 9000);
 
-                int serverCount = 0, count = 0;
-                System.out.println("Server " + count + ": ");
-                while(in.hasNextLine())
-                {
-                    if(count == 5) {
-                        serverCount++;
-                        System.out.println("Server " + serverCount + ": ");
+			// get the input and output streams
+			os = clientSocket.getOutputStream();
+			is = clientSocket.getInputStream();
+			Scanner in = new Scanner(is);
+			PrintWriter out = new PrintWriter(os, true /* autoflush */);
+			Scanner userIn = new Scanner(System.in);
 
-                        count = 0;
-                    }
-                    String line = in.nextLine();
-                    count++;
-                    System.out.println(line);
-                }
-            }
-            finally
-            {
-                clientSocket.close();
-            }
+			out.print(ci.retrieveIdentifier() + "#");
+			for (String fileName : ci.retrieveFileList())
+				out.print(fileName);
+			out.println("#joining");
 
-        }
-        catch(IOException ioe)
-        {
-            System.err.println("Couldn't connect to server");
-            System.exit(0);
-        }
-    }
+			String userLine = null;
+			System.out.println("Enter command to send to the SERVER");
+			do {
+				userLine = userIn.nextLine();
+
+				String[] input = userLine.split(" ");
+
+				if (input[0].equalsIgnoreCase("get")) {
+					System.out.println("Requesting file " + input[1]
+							+ " from server");
+					out.println(ci.retrieveIdentifier() + "#" + input[0] + "#"
+							+ input[1]);
+					// do something with client-client connection to retrieve file
+				} else if (input[0].equalsIgnoreCase("add")) {
+					if (ci.addFile(input[1])) {
+						System.out.println("Broadcasting file " + input[1]
+								+ " to server");
+
+						out.println(ci.retrieveIdentifier() + "#" + input[0]
+								+ "#" + input[1]);
+					}
+				} else if (!input[0].equalsIgnoreCase("exit")) {
+					System.err.println("Unrecognized command");
+				}
+			} while (!userLine.equalsIgnoreCase("exit"));
+			out.println(ci.retrieveIdentifier() + "#leaving");
+			userIn.close();
+			in.close();
+			clientSocket.close();
+			System.out.println("Terminating program");
+
+		}
+
+		catch (IOException ioe) {
+			System.err
+					.println("Couldn't create socket or could not connect to server");
+			System.exit(0);
+		}
+	}
 }
